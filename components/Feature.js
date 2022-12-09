de571272120b8d86c5b2bf7c5f3f5288b020f44b
42elenz <your_github_email>
2022-12-09 15:22:22 +0100

    changed layout for mobile phone check for mobile

--- components/Feature.js
+++ components/Feature.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {useRouter} from "next/router";
 import { Container, Row, Col } from "reactstrap";
 const FeatureBox = (props) => {
+	const router = useRouter();
+	const isMobile = router.query.mobile;
   return (
     <>
     {
+	!isMobile &&
+	(
       props.features.map((feature, key) =>
       (feature.id % 2 !== 0) ?
         <Row key={key} className={feature.id === 1 ? "align-items-center" : "align-items-center mt-5"}>
@@ -17,7 +22,7 @@ const FeatureBox = (props) => {
                 <div className="my-4">
                   <i className={feature.icon}></i>
                 </div>
-                <h5 className="text-dark font-weight-normal mb-3 pt-3">{feature.title}</h5>
+                <h5 className="text font-weight-normal mb-3 pt-3">{feature.title}</h5>
                 <p className="text-muted mb-3 f-15">{feature.desc}</p>
               </div>
             </Col>
@@ -39,8 +44,31 @@ const FeatureBox = (props) => {
           </div>
         </Col>
       </Row>
+	  )
       )
-    }
+	
+	}
+	{
+	isMobile &&
+	(
+	props.features.map((feature, key) =>
+      (feature.id % 2 !== 0) ?
+        <Row key={key} className={feature.id === 1 ? "align-items-center" : "align-items-center mt-5"}>
+          <Col md={5} >
+            <div>
+              <img src={feature.img} alt="" className="img-fluid d-block mx-auto" width="350px"/>
+            </div>
+          </Col>
+        </Row>
+	:
+        <Col md={{size:5, offset:1}} className="mt-5 mt-sm-0">
+          <div>
+            <img src={feature.img} alt="" className="img-fluid d-block mx-auto" width='350px'/>
+          </div>
+        </Col>
+	)
+	)
+	}
     </>
   );
 }
@@ -48,9 +76,9 @@ const FeatureBox = (props) => {
 const Feature = () => {
 const features = [
     {id : 1, img : "/assets/Medikamentwahl.png", title : "Auswahl des Medikaments", desc : "Wählen Sie ein Medikament aus dem Drop-Down Menu, oder geben Sie die Werte einfach selber ein"},
-    {id : 1, img : "/assets/Hwz.png", title : "Halbwertszeit", desc : "Wählen Sie Halbwertszeit in Stunden aus oder nutzen Sie automatische Befüllung"},
+    {id : 2, img : "/assets/Hwz.png", title : "Halbwertszeit", desc : "Wählen Sie Halbwertszeit in Stunden aus oder nutzen Sie automatische Befüllung"},
     {id : 1, img : "/assets/Verabreichungsintro.png", title : "Verabreichung", desc : "In welchen Abständen verabreichen Sie die Medikation?"},
-	{id : 1, img : "/assets/Konzentration.png", title : "Gemessene Konzentration", desc : "Wie hoch war die Konzentration im Blut ('Spiegel') zum Zeitpunkt der Messung?"},
+	{id : 2, img : "/assets/Konzentration.png", title : "Gemessene Konzentration", desc : "Wie hoch war die Konzentration im Blut ('Spiegel') zum Zeitpunkt der Messung?"},
 	{id : 1, img : "/assets/Zeiten.png", title : "Eintragen der Daten", desc : "Tragen Sie die Zeiten ein. Achten Sie darauf, dass die Chronologie gewahrt bleibt!"}
   ];
 return (
